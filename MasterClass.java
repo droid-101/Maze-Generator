542a5267e55b08fca54b77faccde85c151849ca7
droid-101 <shanepinto18@yahoo.com>
2020-05-23 15:59:41 -0400

    Minecraft Maze Generation
    - Program generates commands for maze generation in minecraft.
    - Program writes commands to function files.
    - Program now allows user to add a height dimension to maze.

--- MasterClass.java
+++ MasterClass.java
@@ -1,19 +1,88 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 class MasterClass
 {
     public static void main(String [] arg)
     {
-        int [][] maze = Generator.generateMaze(9, 9, false);
+        int wallHeight = 2;
 
-        for (int i = 0; i < Maze.numColumns(maze); i++)
+        int [][] maze = Generator.generateMaze(20, 20, false);
+
+        for (int i = Maze.numColumns(maze) - 1; i > 0; i--)
         {
             if (maze[Maze.numRows(maze) - 2][i] != 0)
             {
                 maze[Maze.numRows(maze) - 1][i] = 1;
-                i = Maze.numColumns(maze);
+                i = 0;
             }
         }
 
         Solver.check(maze);
-        Maze.print(maze);
+
+        try
+        {
+            writeGenerate(maze, wallHeight);
+        }
+        catch (IOException e)
+        {
+            System.out.println("ERROR");
+        }
+
+        try
+        {
+            clear(maze, wallHeight);
+        }
+        catch (IOException e)
+        {
+            System.out.println("ERROR");
+        }
+    }
+
+
+    public static void writeGenerate(int [][] maze, int wallHeight) throws IOException
+    {
+        String genData = null;
+        FileWriter genWrite = null;
+
+        genWrite = new FileWriter("C:\\Users\\The Pintos\\Documents\\My Games\\Minecraft\\New Minecraft\\data\\.minecraft\\saves\\Maze Runner\\datapacks\\MazeRunner\\data\\maze\\functions\\generate.mcfunction");
+
+        for (int k = 0; k < wallHeight; k++)
+        {
+             for (int j = 0; j < Maze.numColumns(maze); j++)
+            {
+                for (int i = 0; i < Maze.numRows(maze); i++)
+                {
+                    if (maze[i][j] == 0)
+                    {
+                        genData = String.format("setblock %d %d %d minecraft:stone_bricks\n", j, k + 4, i);
+                        genWrite.write(genData);
+                    }
+                }
+            }
+        }
+
+        genWrite.close();
+    }
+
+    public static void clear(int [][] maze, int wallHeight) throws IOException
+    {
+        String clearData = null;
+        FileWriter clearWrite = null;
+
+        clearWrite = new FileWriter("C:\\Users\\The Pintos\\Documents\\My Games\\Minecraft\\New Minecraft\\data\\.minecraft\\saves\\Maze Runner\\datapacks\\MazeRunner\\data\\maze\\functions\\clear.mcfunction");
+
+        for (int k = 0; k < wallHeight; k++)
+        {
+            for (int j = 0; j < Maze.numColumns(maze); j++)
+            {
+                for (int i = 0; i < Maze.numRows(maze); i++)
+                {
+                    clearData = String.format("setblock %d %d %d minecraft:air\n", j, k + 4, i);
+                    clearWrite.write(clearData);
+                }
+            }
+        }
+        clearWrite.close();
     }
 }
